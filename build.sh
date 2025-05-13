2dcd3a0f8428dc812f62338351e82d8845c37e53
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-05-13 00:22:06 -0700

    no quedaeeee

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py migrate
-
-DJANGO_SETTINGS_MODULE=proyectoSeguridad.settings python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
