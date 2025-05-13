642d45b53e70a246f893b931e463afdb75eb36d5
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-05-13 00:06:56 -0700

    no queda2

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Instalando dependencias..."
 pip install -r requirements.txt
 
-echo "Aplicando migraciones..."
 python manage.py migrate
 
-echo "Recolectando archivos estáticos..."
-export DJANGO_SETTINGS_MODULE=proyectoSeguridad.settings
-python manage.py collectstatic --noinput
+DJANGO_SETTINGS_MODULE=proyectoSeguridad.settings python manage.py collectstatic --noinput
